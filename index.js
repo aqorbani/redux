cf996e424390484ebf072554919036a682c68a7f
Amir Hossein Qorbani <a.qorbani.eng@gmail.com>
2024-02-23 14:45:56 +0330

    create reducer

--- index.js
+++ index.js
@@ -4,3 +4,22 @@ function incrementCounter() {
     type: "INCREMENT",
   };
 }
+
+//initial state
+const initialState = {
+  counter: 0,
+  number: 5,
+};
+
+//reducer
+const counterReducer = (state, action) => {
+  switch (action.type) {
+    case "INCREMENT_COUNTER":
+      return {
+        ...state,
+        counter: counter + 1,
+      };
+    default:
+      return state;
+  }
+};
